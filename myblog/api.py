8cfbf1bea5a440fe05a9148bfcaf14657928e2ec
AliHassani-128 <alihassani128@yahoo.com>
2021-08-17 11:31:59 +0430

    add edit post and delete post and edit profile of user

--- myblog/api.py
+++ myblog/api.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, session
+import json
+
+from bson import ObjectId
+from flask import Blueprint, session, jsonify
 from flask import flash
 from flask import g
 from flask import redirect
@@ -7,10 +10,19 @@ from flask import request
 from flask import url_for
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
+
+from myblog.blog import login_required
 from myblog.db import get_db
 
+bp = Blueprint("api", __name__,url_prefix='/api/')
 
-bp = Blueprint("api", __name__)
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 
 
 @bp.route("/posts_list/")
@@ -19,21 +31,33 @@ def list_post():
 
 
 
-@bp.route("/post-delete/<post_id>/")
-def post_delete(post_id):
-    return render_template('')
 
+@bp.route("/delete-post/<post_id>")
+@login_required
+def delete_post(post_id):
+    get_db().posts.delete_one({'_id':ObjectId(post_id)})
+    db = get_db()
+    posts = db.posts.find()
+    return render_template('all_posts.html', posts=list(posts))
 
 
 @bp.route("/post-deactive/<post_id>/")
 def post_deactive(post_id):
-    return render_template('')
+    get_db().posts.update({'_id': ObjectId(post_id)},{'$set':{'status':False}})
+    db = get_db()
+    post = db.posts.find()
+    return render_template('all_posts.html', posts=list(post))
 
+@bp.route("/post-active/<post_id>/")
+def post_active(post_id):
+    get_db().posts.update({'_id': ObjectId(post_id)}, {'$set': {'status': True}})
+    db = get_db()
+    post = db.posts.find()
+    return render_template('all_posts.html', posts=list(post))
 
 @bp.route("/categorys-list/")
 def list_categorys():
-    sub_categories=get_db().subcategory.find()
-    return render_template('test.html',categories=sub_categories)
+    return render_template('')
 
 
 @bp.route("/tags-list/")
