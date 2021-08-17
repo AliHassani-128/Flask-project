d9df1600769e3cece3bb2fc807a676e212339bab
mohammad <aliakbariiimohammad@gmail.com>
2021-08-17 16:59:14 +0430

    new version of project(just should do category pages and search)

--- myblog/api.py
+++ myblog/api.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, session
+import json
+
+from bson import ObjectId
+from flask import Blueprint, session, jsonify
 from flask import flash
 from flask import g
 from flask import redirect
@@ -7,10 +10,18 @@ from flask import request
 from flask import url_for
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
+
+from myblog.blog import login_required
 from myblog.db import get_db
 
+bp = Blueprint("api", __name__, url_prefix='/api/')
 
-bp = Blueprint("api", __name__)
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 
 @bp.route("/posts_list/")
@@ -18,26 +29,39 @@ def list_post():
     return render_template('')
 
 
-
-@bp.route("/post-delete/<post_id>/")
-def post_delete(post_id):
-    return render_template('')
-
+@bp.route("/delete-post/<post_id>")
+@login_required
+def delete_post(post_id):
+    get_db().posts.delete_one({'_id': ObjectId(post_id)})
+    db = get_db()
+    posts = db.posts.find()
+    return render_template('all_posts.html', posts=list(posts))
 
 
 @bp.route("/post-deactive/<post_id>/")
 def post_deactive(post_id):
-    return render_template('')
+    get_db().posts.update({'_id': ObjectId(post_id)},
+                          {'$set': {'status': False}})
+    db = get_db()
+    post = db.posts.find()
+    return render_template('all_posts.html', posts=list(post))
+
+
+@bp.route("/post-active/<post_id>/")
+def post_active(post_id):
+    get_db().posts.update({'_id': ObjectId(post_id)},
+                          {'$set': {'status': True}})
+    db = get_db()
+    post = db.posts.find()
+    return render_template('all_posts.html', posts=list(post))
 
 
 @bp.route("/categorys-list/")
 def list_categorys():
-    categories=get_db().categories.find()
+    categories = get_db().categories.find()
+    subcategories = get_db().subcategories.find()
 
-
-    subcategories=get_db().subcategories.find()
-
-    return render_template('test.html',categories=categories,subcategories=subcategories)
+    return render_template('test.html', categories=categories, subcategories=subcategories)
 
 
 @bp.route("/tags-list/")
@@ -45,11 +69,9 @@ def list_tags():
     return render_template('')
 
 
-
 @bp.route("/search/")
 def search():
     return render_template('')
-
 
 
 @bp.route("/user-profile/<user_id>/")
--- myblog/blog.py
+++ myblog/blog.py
@@ -1,5 +1,4 @@
 import json
-from bson import ObjectId
 import functools
 from flask import Blueprint, session
 from flask import flash
@@ -8,7 +7,6 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
@@ -42,14 +40,8 @@ def login_required(view):
     return wrapped_view
 
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
 
-
-@bp.route("/home")
+#Home page of site that show all posts
 @bp.route("/")
 def home():
     db = get_db()
@@ -59,11 +51,13 @@ def home():
     return render_template('all_posts.html', posts=list(posts),categories=categories,subcategories=list(subcategories))
 
 
+#for showing all detail of one post
 @bp.route("/post/<post_id>/")
 def post(post_id):
     db = get_db()
     post = db.posts.find({'_id': ObjectId(post_id)})
     return render_template('detail_post.html', posts=list(post))
+
 
 
 @bp.route("/category-posts/<category_id>/")
@@ -76,6 +70,8 @@ def tag(tag_id):
     return render_template('')
 
 
+
+#for register a new user to site
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -85,21 +81,22 @@ def register():
         email = request.form.get('email')
         phone = request.form.get('phone')
         image = request.files.get('image')
+        error = None
         if image:
             image.save('myblog/static/media/uploads/profiles/' +
                        secure_filename(image.filename))
         else:
-            flash('عکس برای پروفایلتان انتخاب نکردید!')
 
+            error = 'عکس برای پنل کاربری خود انتخاب نکردید'
         db = get_db()
-        error = None
+
 
         if not username:
             error = "نام کاربری الزامی است"
         elif not password:
             error = "رمز عبور الزامی است"
         elif db.user.find_one({"username": username}) is not None:
-            error = f"استفاده شده است.لطفا نام دیگری انتخاب کنید{username}نام کاربری"
+            error = f"وجود دارد.لطفا نام دیگری انتخاب کنید {username} نام کاربری"
 
         if error is None:
             # the name is available, store it in the database and go to
@@ -107,13 +104,14 @@ def register():
             user = {'username': username, 'password': generate_password_hash(password),
                     'email': email, 'phone': phone, 'image': image.filename}
             db.user.insert_one(user)
+            flash('پنل کاربری شما با موفقیت ثبت شد','alert-success')
             return redirect(url_for("blog.login"))
         else:
             flash(error, "alert-danger")
 
     return render_template("auth/register.html")
 
-
+#for login users who were registered
 @bp.route("/login", methods=("GET", "POST"))
 def login():
     """Log in a registered user by adding the user id to the session."""
@@ -141,17 +139,83 @@ def login():
             session.clear()
             user['_id'] = str(user['_id'])
             session["user_id"] = user["_id"]
-            flash(f"عزیز،خوش امدید!{user['username']}", "alert-success")
+            flash(f"عزیز،خوش آمدید {user['username']} ", "alert-success")
             return redirect(url_for("blog.home"))
         else:
             flash(error, "alert-danger")
 
     return render_template("auth/login.html")
 
-
+#for shoiwng all posts that a user with id (user_id) was written
 @bp.route('/user-posts/<user_id>/')
 def user_posts(user_id):
-    user = get_db().user.find({'_id': ObjectId(user_id)})
-    posts = get_db().posts.find({'user._id': user[0]['_id']})
+    posts = get_db().posts.find({'user._id': ObjectId(user_id)})
     return render_template('all_posts.html', posts=list(posts))
+
+
+#for shoiwng all posts that are with a common tag
+@bp.route('/tag-posts/<tag>/')
+def tag_posts(tag):
+    posts = get_db().posts.find({'tag': {'$in':[tag]}})
+    return render_template('all_posts.html', posts=list(posts))
+
+#like a post
+@bp.route('/like/',methods=['POST'])
+def like():
+
+    post_id = request.args.get('post_id')
+    user_id = request.args.get('user_id')
+
+    post = get_db().posts.find_one({'_id':ObjectId(post_id)})
+
+    if user_id :
+        if ObjectId(user_id) not in post['like']:
+
+            get_db().posts.update({ '_id':ObjectId(post_id)},{ '$push': { 'like': ObjectId(user_id)} })
+            likes= list(get_db().posts.aggregate(
+                                           [{'$match':{'_id':ObjectId(post_id)}},
+                                               {'$project':
+                                                 {'like':{'$size':'$like'}}}
+                                            ]))[0]
+            return json.dumps({'likes':likes['like'],'color':'red'})
+        else:
+            get_db().posts.update({'_id': ObjectId(post_id)}, {'$pull': {'like': ObjectId(user_id)}})
+            likes = list(get_db().posts.aggregate(
+                [{'$match': {'_id': ObjectId(post_id)}},
+                 {'$project':
+                      {'like': {'$size': '$like'}}}
+                 ]))[0]
+            return json.dumps({'likes': likes['like'],'color':'lightslategray'})
+    else:
+        return json.dumps({'error': 'برای لایک کردن پست ها باید کاربر سایت باشید'})
+
+#dislike a post
+@bp.route('/dislike/',methods=['POST'])
+def dislike():
+    post_id = request.args.get('post_id')
+    user_id = request.args.get('user_id')
+
+    post = get_db().posts.find_one({'_id': ObjectId(post_id)})
+
+    if user_id:
+        if ObjectId(user_id) not in post['dislike']:
+
+            get_db().posts.update({'_id': ObjectId(post_id)}, {'$push': {'dislike': ObjectId(user_id)}})
+            dislikes = list(get_db().posts.aggregate(
+                [{'$match': {'_id': ObjectId(post_id)}},
+                 {'$project':
+                      {'dislike': {'$size': '$dislike'}}}
+                 ]))[0]
+            return json.dumps({'dislikes': dislikes['dislike']})
+        else:
+            get_db().posts.update({'_id': ObjectId(post_id)}, {'$pull': {'dislike': ObjectId(user_id)}})
+            dislikes = list(get_db().posts.aggregate(
+                [{'$match': {'_id': ObjectId(post_id)}},
+                 {'$project':
+                      {'dislike': {'$size': '$dislike'}}}
+                 ]))[0]
+            return json.dumps({'dislikes': dislikes['dislike']})
+    else:
+        return json.dumps({'error': 'برای نپسندیدن پست ها باید کاربر سایت باشید'})
+
 
